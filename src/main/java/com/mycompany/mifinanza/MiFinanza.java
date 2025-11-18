e879f8a5df1c168c82af2b8bbafdf6b50b4d9c9b
Federico <federico.luque.santos.al@iespoligonosur.org>
2025-11-18 16:38:00 +0100

    Estructura inicial MVC y .gitignore creados

---
+++ src/main/java/com/mycompany/mifinanza/MiFinanza.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.mifinanza;
+
+/**
+ *
+ * @author alumnadotarde
+ */
+public class MiFinanza {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
